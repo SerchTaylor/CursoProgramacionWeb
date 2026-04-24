0a05c925e7069980482ffaa9d4afbf2f1b565859
Serch Taylor <sersasfer@gmail.com>
2026-04-24 16:57:40 +0200

    24abr1657

---
+++ JavaScript/Tema1/nose.js
@@ -0,0 +1,69 @@
+/* Con JavaScript dentro de HTML puedes:
+
+    Manipular el contenido de la página
+
+    Cambiar estilos
+
+    Crear interactividad
+
+    Manejar formularios
+
+    Hacer animaciones
+
+    Conectar APIs
+
+    Crear juegos
+
+    Construir aplicaciones completas
+
+JavaScript es el motor que hace que las páginas web sean dinámicas.
+
+¿Qué es una API?
+
+Una API (Application Programming Interface) es un puente que permite que dos programas se comuniquen entre sí.
+
+Es como un mesero en un restaurante:
+
+    Tú → haces un pedido
+
+    El mesero (API) → lleva tu pedido a la cocina
+
+    La cocina (servidor) → prepara la comida
+
+    El mesero → te trae la comida
+
+Tú no entras a la cocina, ni sabes cómo cocinan.
+Solo pides algo y recibes una respuesta.
+
+Eso es exactamente una API.
+🧩 ¿Para qué sirve una API?
+
+Sirve para:
+
+    Obtener datos de internet
+
+    Enviar información a un servidor
+
+    Conectar tu web con servicios externos
+
+    Hacer apps dinámicas*/
+
+/* This is and a comment */
+
+/* VAriables */
+// Var es the old form for create a variable i s'acaba en ;
+var variable = 0 // no se usa
+
+let variable2 ="viernes"
+
+let variable_1 = "Hola mundo en JavaScript";
+
+console.log("Primer mensaje en JS que es:",variable_1,"y esta es la variable",variable);
+
+
+const LENGUAJE = 'JavaScript'
+const PI = 3.1416
+
+console.log(variable2)
+
+console.log(LENGUAJE,PI)
