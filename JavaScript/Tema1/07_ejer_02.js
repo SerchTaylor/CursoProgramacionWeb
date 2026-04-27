9507543d429613914c9203b11c2027efafc5c27e
Serch Taylor <sersasfer@gmail.com>
2026-04-27 19:01:34 +0200

    27abr1901

---
+++ JavaScript/Tema1/07_ejer_02.js
@@ -0,0 +1,32 @@
+let diaDiaSemana
+let dia = new Date()
+let diaCorrecto = false
+
+switch(diaDiaSemana){
+    case "27":
+        alert("Hoy es 27")
+        break;
+    case "28":
+        alert("Hoy es 28")
+        break;
+    case "29":
+        alert("Hoy es 29")
+        break;
+    case "30":
+        alert("Hoy es 30")
+        break;
+    case "1":
+        alert("Hoy es 1")
+        break;
+    case "2":
+        alert("Hoy es 22")
+        break;
+    case "3":
+        alert("Hoy es 3")
+        break;
+    default:
+        alert("Pon otra cosa")
+        break
+
+
+}
