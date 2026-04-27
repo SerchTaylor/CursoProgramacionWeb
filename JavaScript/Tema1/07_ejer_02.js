efaf3edf000954483d3cbdfdcd94fafcabd94190
Serch Taylor <sersasfer@gmail.com>
2026-04-27 19:27:10 +0200

    27abr1927

--- JavaScript/Tema1/07_ejer_02.js
+++ JavaScript/Tema1/07_ejer_02.js
@@ -2,31 +2,48 @@ let diaDiaSemana
 let dia = new Date()
 let diaCorrecto = false
 
+
+while (diaCorrecto == false){
+
+diaDiaSemana = prompt("Dime que numero de dia es?")
+diaDiaSemana = diaDiaSemana.toLocaleLowerCase()
+diaDiaSemana.Date()
+
 switch(diaDiaSemana){
-    case "27":
-        alert("Hoy es 27")
+    
+    case"lunes":
+        alert("Es Lunes")
+        diaCorrecto = true
         break;
-    case "28":
+    case "martes":
         alert("Hoy es 28")
+        diaCorrecto = true 
         break;
-    case "29":
+    case "miércoles":
+    case "miercoles":
         alert("Hoy es 29")
+        diaCorrecto = true
         break;
-    case "30":
+    case "jueves":
         alert("Hoy es 30")
+        diaCorrecto = true
         break;
-    case "1":
+    case "viernes":
         alert("Hoy es 1")
+        diaCorrecto = true
         break;
-    case "2":
+    case "sábado":
+    case "sabado":
         alert("Hoy es 22")
+        diaCorrecto = true
         break;
-    case "3":
+    case "domingo":
         alert("Hoy es 3")
+        diaCorrecto = true
         break;
     default:
         alert("Pon otra cosa")
-        break
-
-
+        diaCorrecto = false
+        break;
+}
 }
