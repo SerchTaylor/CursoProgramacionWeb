09648602076091e6aa1c19e6cc66e509bb6ef0ac
Serch Taylor <sersasfer@gmail.com>
2026-04-27 17:27:53 +0200

    27abr1727

--- JavaScript/Tema1/06_ejer_01.js
+++ JavaScript/Tema1/06_ejer_01.js
@@ -3,7 +3,8 @@ console.log("Hola");
 // Alerta te sale un pop-up y hay que aceptar
 alert("Buenas tardes")
 
-//
+// Confirmar una pregunta y se guarda en una variable
+let mayorEdad = confirm("Eres mayor de edad?")
 
-
-//
+// 
+prompt("Dime que numero de dia es?")
