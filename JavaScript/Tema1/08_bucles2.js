f62397a8c82dd19715861d51d54c476fd5721e4c
Serch Taylor <sersasfer@gmail.com>
2026-04-28 19:39:56 +0200

    28abr1939

--- JavaScript/Tema1/08_bucles2.js
+++ JavaScript/Tema1/08_bucles2.js
@@ -14,13 +14,25 @@ for( lletra of texto){
         };
 }
 
-/*
-if (texto2[texto2.length] != " "){
-    contador2 = contador2 +1
-}*/ 
+if (texto.length>0){
+    contador = contador +1
+} 
 
 console.log(contador);
 
-let palabras = texto2.split(/\s+/).length;
+let palabras = texto.split(/\s+/).length;
 
 console.log("Palabras:", palabras);
+
+let num = 0
+let num1 = 0
+let tabla = 6
+for (num;num<=10;num++){
+    resul= tabla * num
+    console.log(tabla+" por "+num+" es "+resul)
+}
+while(num1!=11){
+    resul= tabla * num1
+    console.log(tabla+" por "+num1+" es "+resul)
+    num1= num1 + 1
+}
